3d80094df5d4651d2ad4300842740055170d6236
rayyanshady83 <rayyanisthebest7@gmail.com>
2025-01-19 15:01:53 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
+}
